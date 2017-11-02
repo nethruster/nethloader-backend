a8c2c2063618d29db80d2ecd36aff325b71553cf
Claudio4 <me@claudio4.com>
2017-11-02 22:30:35 +0000

    Modify utils/save-image to return a Promise

--- src/utils/save-image.js
+++ src/utils/save-image.js
@@ -2,16 +2,18 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = function (id, extension, image) {
-  fs.open(path.join(__dirname, "..", "..", "images", `${id}.${extension}`), 'w', (err, fd) => {
-    if (err) {
-      throw 'could not open file: ' + err;
-    }
-
-    fs.write(fd, image, 0, image.length, null, function (err) {
-      if (err) throw 'error writing file: ' + err;
-      fs.close(fd, function () {
-        console.log('wrote the file successfully');
+  return new Promise((resolve, reject) => {
+    fs.open(path.join(__dirname, "..", "..", "images", `${id}.${extension}`), 'w', (err, fd) => {
+      if (err) {
+        return reject(err);
+      }
+  
+      fs.write(fd, image, 0, image.length, null, function (err) {
+        if (err) return reject(err);
+        fs.close(fd, function () {
+          return resolve();
+        });
       });
-    });
-  })
+    })
+  });
 }
