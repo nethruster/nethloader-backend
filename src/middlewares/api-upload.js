20318cb0cd07eab6ef2163391b58c8d1943aae12
Claudio4 <me@claudio4.com>
2017-12-31 21:14:49 +0000

    Add ShareX-like programs compatibility

--- src/middlewares/api-upload.js
+++ src/middlewares/api-upload.js
@@ -1,5 +1,8 @@
 const db = require('./models')
 const addImage = require('../utils/add-image')
+const {getConfigSection} = require('../utils/config')
+
+const publicDomian = (getConfigSection('server')).publicDomian
 
 module.exports = async function (req, res) {
   let key = req.headers['api-key']
@@ -8,8 +11,26 @@ module.exports = async function (req, res) {
     let user = await db.User.findOne({where: {apiKey: key}})
     if (!user) return res.send(400, {success: false, error: 'Invalid api-key'})
     let img = await addImage(user, req.files[0])
+    let body;
+    switch(req.headers['output-format']) {
+      case 'rawLink':
+        body = `${publicDomian}/images/${img.id}`
+        break
+      case 'rawDirectLink':
+        body = `${publicDomian}/${img.id}`
+        break
+      default:
+        body = {
+          success: true,
+          data: {
+            link: `${publicDomian}/${img.id}`,
+            directLink: `${publicDomian}/images/${img.id}`
+          } 
+        }
+        break
+    }
 
-    return res.send(201, {success: true})
+    return res.send(201, body)
   } catch (err) {
     console.error(err)
     return res.sendStatus(500)
