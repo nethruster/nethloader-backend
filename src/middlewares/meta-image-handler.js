4cefcab17f054de5e028a0f549c0d17e3086851a
Claudio4 <me@claudio4.com>
2018-03-18 17:35:38 +0000

    Adds prefeteched data to the image view

--- src/middlewares/meta-image-handler.js
+++ src/middlewares/meta-image-handler.js
@@ -9,21 +9,13 @@ const indexFile = fs.readFileSync(`${serverConfig.clientPath}/index.html`, 'utf8
 module.exports = function (req, res) {
   db.Image.findOne({ where: { id: req.params.id } })
     .then(img => {
+      let imgUrl = `serverConfig.publicDomain}/media/${img.id}.${img.extension}`
       return res.send(indexFile.replace('<title>Nethloader</title>',
-        `<title>Screenshot hosted with Nethloader</title>
-        <meta name="twitter:card" content="photo">
-        <meta name="twitter:title" content="Hosted media">
-        <meta name="twitter:site" content="@nethruster">
-        <meta name="twitter:description" content="Hosted with Nethloader">
-        <meta name="twitter:image:src" content="${serverConfig.publicDomain}/media/${img.id}.${img.extension}">
-        <meta property="og:site_name" content="Nethloader">
-        <meta property="og:title" content="Hosted media">
-        <meta property="og:image" content="${serverConfig.publicDomain}/media/${img.id}.${img.extension}">
-        <meta property="og:description" content="Hosted with Nethloader">
-        <meta property="og:url" content="${serverConfig.publicDomain}/${img.id}">
-        `))
+        `<title>Screenshot hosted with Nethloader</title><meta name="twitter:card" content="photo"><meta name="twitter:title" content="Hosted media"><meta name="twitter:description" content="Hosted with Nethloader"><meta name="twitter:image:src" content="${imgUrl}"><meta property="og:site_name" content="Nethloader"><meta property="og:title" content="Hosted media"><meta property="og:image" content="${imgUrl}"><meta property="og:description" content="Hosted with Nethloader"><meta property="og:url" content="${serverConfig.publicDomain}/${img.id}">`)
+        .replace('<body>', `<body><script>var ssrData = {found: true, id: "${img.id}", extension: "${img.extension}"}</script>`)
+      )
     })
     .catch(() => {
-      return res.status(404).send(indexFile)
+      return res.status(404).send(indexFile.replace('<body>', '<body><script>var ssrData = {found: false, id: null, extension: null}</script>'))
     })
 }
