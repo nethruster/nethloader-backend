4f450ad05394800ebb9c68544f59fd46c699d9aa
gariasf <gariasfa7@alumnes.ub.edu>
2018-03-18 14:04:05 +0100

    Add more SEO meta tags

--- src/middlewares/meta-image-handler.js
+++ src/middlewares/meta-image-handler.js
@@ -10,7 +10,18 @@ module.exports = function (req, res) {
   db.Image.findOne({ where: { id: req.params.id } })
     .then(img => {
       return res.send(indexFile.replace('<title>Nethloader</title>',
-        `<title>Screenshot hosted with Nethloader</title><meta property="og:image" content="${serverConfig.publicDomain}/media/${img.id}.${img.extension}" />`))
+        `<title>Screenshot hosted with Nethloader</title>
+        <meta name="twitter:card" content="photo">
+        <meta name="twitter:title" content="Hosted media">
+        <meta name="twitter:site" content="@nethruster">
+        <meta name="twitter:description" content="Hosted with Nethloader">
+        <meta name="twitter:image:src" content="${serverConfig.publicDomain}/media/${img.id}.${img.extension}">
+        <meta property="og:site_name" content="Nethloader">
+        <meta property="og:title" content="Hosted media">
+        <meta property="og:image" content="${serverConfig.publicDomain}/media/${img.id}.${img.extension}">
+        <meta property="og:description" content="Hosted with Nethloader">
+        <meta property="og:url" content="${serverConfig.publicDomain}/${img.id}">
+        `))
     })
     .catch(() => {
       return res.status(404).send(indexFile)
