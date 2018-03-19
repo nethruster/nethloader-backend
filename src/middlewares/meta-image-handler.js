03987968f9813b033a8b3f76d4bca1955fe80724
gariasf <gariasfa7@alumnes.ub.edu>
2018-03-19 02:44:59 +0100

    Improve meta handler

--- src/middlewares/meta-image-handler.js
+++ src/middlewares/meta-image-handler.js
@@ -4,18 +4,23 @@ const db = require('../models')
 const {getConfigSection} = require('../utils/config')
 const serverConfig = (getConfigSection('server'))
 
+const storage = (getConfigSection('storage'))
+const supportedExtensions = storage.supportedExtensions
+
 const indexFile = fs.readFileSync(`${serverConfig.clientPath}/index.html`, 'utf8')
 
 module.exports = function (req, res) {
   db.Image.findOne({ where: { id: req.params.id } })
     .then(img => {
       let imgUrl = `${serverConfig.publicDomain}/media/${img.id}.${img.extension}`
+      let descContent = `Uploaded ${supportedExtensions.image.includes(img.extension) ? 'image' : 'video'}`
+
       return res.send(indexFile.replace('<title>Nethloader</title>',
-        `<title>Screenshot hosted with Nethloader</title><meta name="twitter:card" content="photo"><meta name="twitter:title" content="Hosted media"><meta name="twitter:description" content="Hosted with Nethloader"><meta name="twitter:image:src" content="${imgUrl}"><meta property="og:site_name" content="Nethloader"><meta property="og:title" content="Hosted media"><meta property="og:image" content="${imgUrl}"><meta property="og:description" content="Hosted with Nethloader"><meta property="og:url" content="${serverConfig.publicDomain}/${img.id}">`)
-        .replace('<body>', `<body><script>var ssrData = {found: true, id: "${img.id}", extension: "${img.extension}", createdAt: ${img.createdAt}}</script>`)
+        `<title>Nethloader - Hosted media</title><meta name="twitter:card" content="photo"><meta name="twitter:title" content="${descContent}"><meta name="twitter:description" content="Self-hosted media via Nethloader"><meta name="twitter:image:src" content="${imgUrl}"><meta property="og:site_name" content="Nethloader"><meta property="og:title" content="${descContent}"><meta property="og:image" content="${imgUrl}"><meta property="og:description" content="Self-hosted media via Nethloader"><meta property="og:url" content="${serverConfig.publicDomain}/${img.id}">`)
+        .replace('<body>', `<body><script>var ssrData = {found: true, id: "${img.id}", extension: "${img.extension}", createdAt: "${img.createdAt}, userId: "${img.user.id}"}</script>`)
       )
     })
-    .catch(() => {
-      return res.status(404).send(indexFile.replace('<body>', '<body><script>var ssrData = {found: false, id: null, extension: null, createdAt: null}</script>'))
+    .catch((err) => {
+      return res.status(404).send(indexFile.replace('<body>', `<body><script>var ssrData = {found: false, id: null, extension: null, createdAt: null, userId: null, err: "${err}"}</script>`))
     })
 }
