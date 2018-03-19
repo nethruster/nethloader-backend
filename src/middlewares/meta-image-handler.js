77f23481b6153a19aa3e427126a0c3ea82fed92f
gariasf <gariasfa7@alumnes.ub.edu>
2018-03-19 16:28:31 +0100

    Fix userId on meta handler

--- src/middlewares/meta-image-handler.js
+++ src/middlewares/meta-image-handler.js
@@ -15,10 +15,12 @@ module.exports = function (req, res) {
       let imgUrl = `${serverConfig.publicDomain}/media/${img.id}.${img.extension}`
       let descContent = `Uploaded ${supportedExtensions.image.includes(img.extension) ? 'image' : 'video'}`
 
-      return res.send(indexFile.replace('<title>Nethloader</title>',
-        `<title>Nethloader - Hosted media</title><meta name="twitter:card" content="photo"><meta name="twitter:title" content="${descContent}"><meta name="twitter:description" content="Self-hosted media via Nethloader"><meta name="twitter:image:src" content="${imgUrl}"><meta property="og:site_name" content="Nethloader"><meta property="og:title" content="${descContent}"><meta property="og:image" content="${imgUrl}"><meta property="og:description" content="Self-hosted media via Nethloader"><meta property="og:url" content="${serverConfig.publicDomain}/${img.id}">`)
-        .replace('<body>', `<body><script>var ssrData = {found: true, id: "${img.id}", extension: "${img.extension}", createdAt: "${img.createdAt}, userId: "${img.user.id}"}</script>`)
-      )
+      return img.getUser().then(user => {
+        return res.send(indexFile.replace('<title>Nethloader</title>',
+          `<title>Nethloader - Hosted media</title><meta name="twitter:card" content="photo"><meta name="twitter:title" content="${descContent}"><meta name="twitter:description" content="Self-hosted media via Nethloader"><meta name="twitter:image:src" content="${imgUrl}"><meta property="og:site_name" content="Nethloader"><meta property="og:title" content="${descContent}"><meta property="og:image" content="${imgUrl}"><meta property="og:description" content="Self-hosted media via Nethloader"><meta property="og:url" content="${serverConfig.publicDomain}/${img.id}">`)
+          .replace('<body>', `<body><script id="meta-script">var ssrData = {found: true, id: "${img.id}", extension: "${img.extension}", createdAt: "${img.createdAt}", userId: "${user.id}"}</script>`)
+        )
+      })
     })
     .catch((err) => {
       return res.status(404).send(indexFile.replace('<body>', `<body><script>var ssrData = {found: false, id: null, extension: null, createdAt: null, userId: null, err: "${err}"}</script>`))
