d5a407ab25d3a98930a75b77d94877fa79b0f711
Claudio Yanes <me@claudio4.com>
2019-06-15 22:11:43 +0100

    bug fix

--- src/utils/verify-login.js
+++ src/utils/verify-login.js
@@ -3,7 +3,7 @@ const db = require('../models')
 
 module.exports = async function (token) {
   let data = await tokenUtils.verify(token)
-  let user = await db.User.findById(data.id)
+  let user = await db.User.findByPk(data.id)
 
   if (user === null) {
     throw new Error('Invalid user')
